f8e245df380936a01482ab7715f1a6a668208d34
Luan-CortesZ <luan.corteszuka@gmail.com>
2025-08-30 14:39:40 +0200

    Make combinator callable

--- midi_part/midi_combinator.py
+++ midi_part/midi_combinator.py
@@ -1,22 +1,22 @@
 import pretty_midi
 
-midi_files = [
-    pretty_midi.PrettyMIDI('./Output/detected_notes0.mid'),
-    pretty_midi.PrettyMIDI('./Output/detected_notes73.mid')
-]
-midi = pretty_midi.PrettyMIDI()
+def combine_midis(piano_file_path, trumpet_file_path, output_file_path):
+    midi_files = [
+        pretty_midi.PrettyMIDI(piano_file_path),
+        pretty_midi.PrettyMIDI(trumpet_file_path)
+    ]
+    combined = pretty_midi.PrettyMIDI()
 
-for midi_file in midi_files:
-    instrument = pretty_midi.Instrument(program=midi_file.instruments[0].program)
-    for note in midi_file.instruments[0].notes:
-        midi_note = pretty_midi.Note(
-            velocity=note.velocity,
-            pitch=note.pitch,
-            start=note.start,
-            end=note.end
-        )
-        instrument.notes.append(midi_note)
-    midi.instruments.append(instrument)
+    for midi_file in midi_files:
+        instrument = pretty_midi.Instrument(program=midi_file.instruments[0].program)
+        for note in midi_file.instruments[0].notes:
+            midi_note = pretty_midi.Note(
+                velocity=note.velocity,
+                pitch=note.pitch,
+                start=note.start,
+                end=note.end
+            )
+            instrument.notes.append(midi_note)
+        combined.instruments.append(instrument)
 
-output_filename = f"./Output/Ecossaise_converted_3.mid"
-midi.write(output_filename)
+    combined.write(output_file_path)
